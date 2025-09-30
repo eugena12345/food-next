f0d4dde61d7fc939431a373e9d8293bd51669cda
eugena <eugena.h12345@gmail.com>
2025-09-30 20:22:17 +0300

    update initial store

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <RootStoreProvider rootStoreInitData={{ theme: 'light' }}>
+        <RootStoreProvider rootStoreInitData={{}}>
           <Header />
           {children}
         </RootStoreProvider>
