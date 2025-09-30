5f227ac34f0be92a869fdb00a31ba5cf76a225d8
eugena <eugena.h12345@gmail.com>
2025-09-30 03:10:20 +0300

    add type for ingredient

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,5 +1,7 @@
 //import type { Ingredient } from '~store/models/recepies';
 
+import { Ingredient } from "~/shared/types/recepies";
+
 export const getNumberCountArr = (pageCount: number): number[] => {
     const result = [];
     for (let i = 1; i <= pageCount; i += 1) {
@@ -8,6 +10,6 @@ export const getNumberCountArr = (pageCount: number): number[] => {
     return result
 };
 
-export const getIngradientsString = (ingArr): string => { //: Ingredient[]
+export const getIngradientsString = (ingArr: Ingredient[]): string => {
     return ingArr.map((ing) => ing.name).join(' + ')
 }
